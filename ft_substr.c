e36cb45f388df6ae539e7f5f9d19fcb62114342b
Victor <vmontoli@student.42barcelona.com>
2023-05-16 07:02:04 +0200

    Felichit'a

--- ft_substr.c
+++ ft_substr.c
@@ -1,50 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_striteri-strmapi.c                              :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vmontoli <vmontoli@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/09 17:26:30 by vmontoli          #+#    #+#             */
-/*   Updated: 2023/05/16 07:00:47 by vmontoli         ###   ########.fr       */
+/*   Created: 2023/05/16 06:43:42 by vmontoli          #+#    #+#             */
+/*   Updated: 2023/05/16 06:49:31 by vmontoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
-void	ft_striteri(char *s, void (*f)(unsigned int, char*))
-{
-	size_t	i;
-
-	if (s == NULL || f == NULL)
-		return ;
-	i = 0;
-	while (s[i] != '\0')
-	{
-		f(i, &s[i]);
-		i++;
-	}
-}
-
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*result;
-	size_t	len;
-	size_t	i;
 
-	if (s == NULL || f == NULL)
+	if (s == NULL)
 		return (NULL);
-	len = ft_strlen(s);
-	result = (char *) malloc(len + 1);
+	result = (char *)ft_calloc(len + 1, sizeof(char));
 	if (result == NULL)
 		return (NULL);
-	i = 0;
-	while (i < (len - 1))
-	{
-		result[i] = f(i, s[i]);
-		i++;
-	}
-	result[len - 1] = '\0';
+	ft_strlcpy(result, s + start, len + 1);
 	return (result);
 }
