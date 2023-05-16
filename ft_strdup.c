433ef5a5590f2205f310c82b3a0c87fac2f693da
Victor <vmontoli@student.42barcelona.com>
2023-05-16 06:36:50 +0200

    Mas seguro de cosas de NULL

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: vmontoli <vmontoli@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 15:09:53 by vmontoli          #+#    #+#             */
-/*   Updated: 2023/05/09 15:09:57 by vmontoli         ###   ########.fr       */
+/*   Updated: 2023/05/16 06:32:23 by vmontoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,12 @@ char	*ft_strdup(const char *s1)
 	size_t	n_bytes;
 	char	*ptr;
 
+	if (s1 == NULL)
+		return (NULL);
 	n_bytes = ft_strlen(s1) + 1;
 	ptr = (char *) malloc(n_bytes);
-	if (ptr != NULL)
-		ft_strlcpy(ptr, s1, n_bytes);
+	if (ptr == NULL)
+		return (NULL);
+	ft_strlcpy(ptr, s1, n_bytes);
 	return (ptr);
 }
