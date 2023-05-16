df5b82e156930bea41f8238eed74f316ec09a912
Victor <vmontoli@student.42barcelona.com>
2023-05-16 02:14:49 +0200

    I io ke se hulio

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vmontoli <vmontoli@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 13:21:57 by vmontoli          #+#    #+#             */
-/*   Updated: 2023/05/09 20:09:01 by vmontoli         ###   ########.fr       */
+/*   Updated: 2023/05/09 20:28:50 by vmontoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+
+/*ft_atoi.c*/
 int		ft_atoi(const char *str);
 
 /*ft_calloc.c*/
