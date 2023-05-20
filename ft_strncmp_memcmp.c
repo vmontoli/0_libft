d2ac4dd89434b8253a45edf1ee396c1feabad267
Victor <vmontoli@student.42barcelona.com>
2023-05-20 04:30:04 +0200

    Arreglado strncmp y memcpm

--- ft_strncmp_memcmp.c
+++ ft_strncmp_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: vmontoli <vmontoli@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 07:44:43 by vmontoli          #+#    #+#             */
-/*   Updated: 2023/05/16 07:52:38 by vmontoli         ###   ########.fr       */
+/*   Updated: 2023/05/20 04:15:36 by vmontoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		result = s1[i] - s2[i];
+		result = (unsigned char) s1[i] - (unsigned char) s2[i];
 		if (result)
 			return (result);
 		if (s1[i] == '\0' || s2[i] == '\0')
@@ -32,14 +32,14 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t		i;
-	const char	*c1;
-	const char	*c2;
-	int			result;
+	size_t				i;
+	const unsigned char	*c1;
+	const unsigned char	*c2;
+	int					result;
 
 	i = 0;
-	c1 = (const char *)s1;
-	c2 = (const char *)s2;
+	c1 = (const unsigned char *)s1;
+	c2 = (const unsigned char *)s2;
 	while (i < n)
 	{
 		result = c1[i] - c2[i];
