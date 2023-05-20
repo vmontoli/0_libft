3b75353f64dd1ad88c1a2767331d0a06a9a58035
Victor <vmontoli@student.42barcelona.com>
2023-05-20 03:49:47 +0200

    Mardito memmove, que ni lo entendia ni he caido en que hago tremendo overflowazo

--- ft_memcpy_memmove.c
+++ ft_memcpy_memmove.c
@@ -6,51 +6,57 @@
 /*   By: vmontoli <vmontoli@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 18:39:41 by vmontoli          #+#    #+#             */
-/*   Updated: 2023/05/19 23:09:43 by vmontoli         ###   ########.fr       */
+/*   Updated: 2023/05/20 03:49:11 by vmontoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/*
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	char	*dst_curr;
-	char	*src_curr;
-	size_t	bytes_copied;
-
-	if (dst == NULL || src == NULL)
-		return (dst);
-	dst_curr = (char *) dst;
-	src_curr = (char *) src;
-	bytes_copied = 0;
-	while (bytes_copied < n)
-	{
-		*(dst_curr++) = *(src_curr++);
-		bytes_copied++;
-	}
-	return (dst);
-}
-*/
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	return (ft_memmove(dst, src, n));
 }
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+static void	*ft_memmove_left(void *dst, const void *src, size_t len)
 {
-	char	*dst_curr;
-	char	*src_curr;
-	size_t	bytes_copied;
+	char		*dst_char;
+	const char	*src_char;
+	size_t		i;
 
-	dst_curr = (char *) dst;
-	src_curr = (char *) src;
-	bytes_copied = 0;
-	while (bytes_copied < len && dst_curr != src && src_curr != dst)
+	dst_char = (char *) dst;
+	src_char = (const char *) src;
+	i = 0;
+	while (i < len)
 	{
-		*(dst_curr++) = *(src_curr++);
-		bytes_copied++;
+		dst_char[i] = src_char[i];
+		i++;
 	}
 	return (dst);
+}
+
+static void	*ft_memmove_right(void *dst, const void *src, size_t len)
+{
+	char		*dst_char;
+	const char	*src_char;
+	size_t		i;
+
+	dst_char = (char *) dst;
+	src_char = (const char *) src;
+	i = len;
+	while (i > 0)
+	{
+		i--;
+		dst_char[i] = src_char[i];
+	}
+	return (dst);
+}
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	if (len == 0 || (dst == src))
+		return (dst);
+	if (dst < src)
+		return (ft_memmove_left(dst, src, len));
+	else
+		return (ft_memmove_right(dst, src, len));
 }
