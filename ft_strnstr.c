107bf02c740ddfbdfed5e234201084e2142c836f
Victor <vmontoli@student.42barcelona.com>
2023-05-20 19:30:46 +0200

    Menudo trunyaco habia montado con strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: vmontoli <vmontoli@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 07:53:42 by vmontoli          #+#    #+#             */
-/*   Updated: 2023/05/16 08:05:06 by vmontoli         ###   ########.fr       */
+/*   Updated: 2023/05/20 19:03:33 by vmontoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,28 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	first_pos_in_haystack;
-	size_t	current_pos_in_needle;
-	size_t	needle_len;
+	size_t	pos0_in_hs;
+	size_t	pos_in_ndl_n_delta_in_hs;
 
-	first_pos_in_haystack = 0;
-	current_pos_in_needle = 0;
-	needle_len = ft_strlen(needle);
-	while (first_pos_in_haystack <= (len - needle_len))
+	if (ft_strlen(needle) == 0)
+		return ((char *)haystack);
+	len = ft_size_t_min(len, ft_strlen(haystack));
+	if (len == 0 || len < ft_strlen(needle))
+		return (NULL);
+	pos0_in_hs = 0;
+	pos_in_ndl_n_delta_in_hs = 0;
+	while (pos0_in_hs <= (len - ft_strlen(needle)))
 	{
-		if (haystack[first_pos_in_haystack + current_pos_in_needle]
-			!= needle[current_pos_in_needle])
+		if (needle[pos_in_ndl_n_delta_in_hs] == '\0')
+			return ((char *)&haystack[pos0_in_hs]);
+		if (haystack[pos0_in_hs + pos_in_ndl_n_delta_in_hs]
+			!= needle[pos_in_ndl_n_delta_in_hs])
 		{
-			first_pos_in_haystack++;
-			current_pos_in_needle = 0;
+			pos0_in_hs++;
+			pos_in_ndl_n_delta_in_hs = 0;
 		}
-		else if (current_pos_in_needle <= needle_len - 1)
-			current_pos_in_needle++;
 		else
-			return ((char *)haystack + first_pos_in_haystack);
+			pos_in_ndl_n_delta_in_hs++;
 	}
 	return (NULL);
 }
